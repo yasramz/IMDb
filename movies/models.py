9c3cb6fd998185122ab094d9e62435a92760a574
yasramz <116145800+yasramz@users.noreply.github.com>
2022-11-23 13:53:11 +0330

    SET movies.models

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,74 @@
+from django.contrib.postgres import constraints
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    title = models.CharField(max_length=50)
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Role(models.Model):
+    title = models.CharField(max_length=50)
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Crew(models.Model):
+    MALE = 1
+    FEMALE = 2
+    GENDER_CHOICES = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female')
+    )
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birthday = models.DateField(null=True, blank=True)
+    gender = models.PositiveSmallIntegerField(choices=GENDER_CHOICES, default=MALE)
+    avatar = models.ImageField(upload_to='crew/avatars/', null=True, blank=True)
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    @property
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    release_date = models.DateField(null=True, blank=True)
+    avatar = models.ImageField(upload_to='movies/avatars/', null=True, blank=True)
+    genres = models.ManyToManyField(Genre, related_name='movies')
+    crew = models.ManyToManyField(Crew, through='MovieCrew', related_name='movies')
+    view_count = models.IntegerField(default=0)
+    is_valid = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class MovieCrew(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='movie_crew')
+    crew = models.ForeignKey(Crew, on_delete=models.CASCADE, related_name='movie_crew')
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, related_name='movie_crew')
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=("movie", "role", "crew"), name="unique_movie_role_crew")]
