6923e50b40397c378f7433a2a35fd02b835d53fd
yasramz <116145800+yasramz@users.noreply.github.com>
2022-11-24 17:17:48 +0330

    SET template and CUSTOMISE related urls and views for movies_list

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from movies.views import movie_list
+
 
 urlpatterns = [
     path('', movie_list, name='movie_list')
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from movies.models import Movie
+
 
 def movie_list(request):
-    pass
+    movies = Movie.objects.all().filter(is_valid=True)[:8]
+    context = {
+        'movies': movies
+    }
+    return render(request, 'movies/movies_list.html', context=context)
